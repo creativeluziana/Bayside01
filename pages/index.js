fc9cd8e53124ee6b3f51dd142b1a24895c751047
LUZIANA <luziana.vellen@gmail.com>
2025-04-14 22:15:05 +0530

    Add Next.js configuration and basic setup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Bayside Sports</title>
+        <meta name="description" content="Bayside Sports Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Bayside Sports</h1>
+      </main>
+    </div>
+  )
+} 
